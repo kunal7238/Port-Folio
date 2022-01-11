b8646e4b3a3e750908183fde70473531d50c4a4d
kunal7238 <kunal.singh_cs19@gla.ac.in>
2022-01-11 19:13:19 +0530

    created project page and card section

---
+++ src/Pages/ProjectPage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { data } from '../assets/data';
+import Card from '../component/Card'
+import Header from '../component/Header'
+import Project from '../component/Project';
+import {Link} from 'react-router-dom';
+
+
+
+function ProjectPage() {
+    return (
+        <div className="bg-primary">
+            <Header/>
+            {/* <Project/> */}
+            <div className="bg-primary">
+            <h1 className="font-bold text-5xl text-center text-white pt-16 ">Something that he has build</h1>
+        <p className="mt-4 text-purple-200 text-center text-lg font-light">
+        with love, expertise and pinch of magical ingredients
+        </p>
+            </div>
+
+{/* Card */}
+            <div className="flex flex-wrap justify-center mt-14">
+                {
+                    data.map((each)=>(
+                         <Link to={`/${each.name}`}>
+                            <Card name={each.name} desc={each.desc}/>
+                        </Link>                       
+                    )) 
+                }        
+            </div>
+        </div>
+    )
+}
+
+export default ProjectPage;
---
+++ src/component/Card.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { VscGithub } from 'react-icons/vsc';
+import {HiOutlineExternalLink} from 'react-icons/hi'
+ 
+function Card({
+    name, desc, github, live
+}) {
+    return (
+        <div className="text-white w-96  bg-gray-500 rounded-lg p-5 flex flex-col border-2 border-gray-500 hover:border-white  justify-between m-2 shadow-lg shadow-gray-600" >
+            <div className="">
+                <h1 className="text-2xl font-bold">{name}</h1>
+                <p>{desc}
+
+                </p>
+            </div>
+
+            {/* <div className=" space-x-3 mt-3 flex text-primary" >
+               {  ( <a href={github} className="font-bold text-sm hover:text-white flex items-center ">GitHub <VscGithub className="ml-1"/></a>
+               )}
+                <a  href={live} className="font-bold text-sm hover:text-white flex items-center">Live <HiOutlineExternalLink className="ml-1"/></a>
+            </div> */}
+        </div>
+    )
+}
+
+export default Card;
